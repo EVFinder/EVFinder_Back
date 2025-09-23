f9a5346d14ddb3ef7ae894284cf9c598c90d12dd
LeeKyungBum <lkb3311@gmail.com>
2025-09-23 16:10:59 +0900

    shareDTO, reserveDTO 필드 추가

--- src/main/java/com/example/backend/chargerbnb/dto/ShareDTO.java
+++ src/main/java/com/example/backend/chargerbnb/dto/ShareDTO.java
@@ -2,6 +2,7 @@ package com.example.backend.chargerbnb.dto;
 
 import com.example.backend.common.json.TimestampDeserializer;
 import com.example.backend.common.json.TimestampSerializer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.cloud.Timestamp;
@@ -11,10 +12,17 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ShareDTO {
-    private String id;
+    
+    @JsonIgnore
+    private String id; // 파이어베이스
+    
     private String address;
     private double lat;
     private double lon;
+
+    private String hostName;    // 호스트 이름
+    private String hostContact; // 호스트 연락처
+    private String stationName; // 충전소 이름
 
     private String chargerType;   // 급속/완속
     private String power;         // 50kW, 100kW 등
