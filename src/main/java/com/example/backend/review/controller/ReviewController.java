64147e59bc83e74beaeac7e13b1f3774b763bd95
LeeHuSeong <dlgntjdsla@gmail.com>
2025-09-24 10:12:33 +0900

    리뷰 추가, 삭제 구현중 아직, userName미흡

---
+++ src/main/java/com/example/backend/review/controller/ReviewController.java
@@ -0,0 +1,54 @@
+package com.example.backend.review.controller;
+
+import com.example.backend.review.dto.ReviewCreateRequest;
+import com.example.backend.review.dto.ReviewResponse;
+import com.example.backend.review.service.ReviewService;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/review")
+public class ReviewController {
+    private final ReviewService reviewService;
+    private final Firestore firestore;
+
+    //리뷰 작성
+    @PostMapping("/add/{uid}")
+    public ReviewResponse add(@PathVariable String uid,
+                              @RequestBody ReviewCreateRequest req) throws Exception {
+        // userName 은 users/{uid} 문서에서 가져옴
+        DocumentSnapshot userSnap = firestore.collection("users").document(uid).get().get();
+        String userName = userSnap.getString("userName");
+        if (userName == null) userName = "사용자";
+
+        return reviewService.add(uid, userName, req);
+    }
+
+
+    //리뷰 삭제
+    @DeleteMapping("/delete/{uid}/{reviewId}")
+    public void delete(@PathVariable String uid,
+                       @PathVariable String reviewId) throws ExecutionException, InterruptedException {
+        reviewService.delete(uid, reviewId);
+    }
+
+    
+    //사용자별 리뷰 조회
+    @GetMapping("/list/user/{uid}")
+    public List<ReviewResponse> listByUser(@PathVariable String uid) throws ExecutionException, InterruptedException {
+        return reviewService.listByUser(uid);
+    }
+
+
+    //충전소별 리뷰 조회
+    @GetMapping("/list/station/{id}")
+    public List<ReviewResponse> listByStation(@PathVariable String id) throws ExecutionException, InterruptedException {
+        return reviewService.listByStation(id);
+    }
+}
