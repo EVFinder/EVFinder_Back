e9f6a1e69fa08f9bb77365773c784ec58cfd461b
LeeKyungbum <lkb3311@gmail.com>
2025-09-25 17:15:56 +0900

    네비게이션 api 구현

---
+++ src/main/java/com/example/backend/navigation/dto/NavigationDTO.java
@@ -0,0 +1,11 @@
+package com.example.backend.navigation.dto;
+
+import lombok.Data;
+
+@Data
+public class NavigationDTO {
+    private double startLat;
+    private double startLon;
+    private double endLat;
+    private double endLon;
+}
